e7b32541c1544dfe04b0dc9a2fccc04c785e1921
Fadika2 Mauro <2000180@afpa.fr>
2020-10-14 11:42:00 +0200

    Corrections
    Corrections des exercices avec Trystan

--- tableau.php
+++ tableau.php
@@ -12,42 +12,55 @@ $a = array("19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "C
     { 
         if($mois== "19002")
         {
-            echo  $cle = array_search("Validation", $valeur )+ 1;
+            // echo permet d'afficher une chaine de caractère.
+            // Il vaut mieux faire l'assignation/l'affectation avant de faire un echo.
+            // echo $cle = array_search("Validation", $valeur )+ 1;
+
+            // Assignation d'une valeur à la variable $cle.
+            $cle = array_search("Validation", $valeur )+ 1;
+
+            // Affichage de la valeur de $cle.
+            echo $cle;
         }
     
     }
 
  // Exo 2
-/*$mem = -1; // variable
+$mem = -1; // variable
 foreach ($a['19001'] as $value => $val)
 {
-if ($val == "Stage")
+    echo $value.': '.$val.'!<br>';
+    if ($val == "Stage")
     {
         $mem = $value;
         
     }
 }
-echo ($mem);  // memoire*/
+echo ($mem);  // memoire
 
 //Exo 3
-/*foreach($a as $k=>$v)
-{
-$i = array_keys($a);
-}
-echo var_dump($i);*/
 
-/*$i = array();
+// Ici deux méthodes différentes pour arriver au même résultat.
+
+
+// foreach($a as $k=>$v)
+// {
+$i = array_keys($a);
+var_dump($i);
+// }
+
+$i = array();
 foreach ($a as $k => $v)
 {
 echo  "$k";
 array_push($i, $k);
+var_dump($i);
 
 }   
-var_dump($i);*/
 
 //Exo 4 
 $t = array ();
-foreach ($a['19003'] as $k =>$v)
+foreach ($a['19003'] as $k => $v)
 {
     if($v == "Stage")
     {
@@ -56,10 +69,6 @@ foreach ($a['19003'] as $k =>$v)
     }
 }
 echo count($t);
-
-
-
-
-
+var_dump($t);
 
     ?>
